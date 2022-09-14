7b299af0151e3ae77fbde9bffc25c2d513dd926d
Didiboycodes <61579857+Iam-Didi@users.noreply.github.com>
2022-09-13 21:27:20 -0700

    Add files via upload

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@
 let questions = [
     {
         numb: 1,
-        question: "what does html stand for?",
+        question: "what does HTML stand for?",
         answer: "Hyper Text Markup Language",
         options: [
             "Hyper Text Preprocessor",
@@ -17,10 +17,166 @@ let questions = [
         question: "What Does CSS Stand for?",
         answer: "Hyper Text Markup Language",
         options: [
-            "common style sheet",
-            "colorful style sheet",
-            "computer style sheet",
-            "cascading style Sheet",
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet",
+        ]
+    },
+ 
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "hypertext preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor",
+        ]
+    },
+  
+    {
+        numb: 4,
+        question: "What does SQL stand For?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question  Language",
+            "Structured Query Language",
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML Stand for?",
+        answer: "Extensible Markup Language",
+        options: [
+            "Extensible Markup Language",
+            "Executable Multiple Language",
+            "Extra Multi-program Language",
+            "Examine Multiple Language",
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Which of these is not a Flex-direction?",
+        answer: "Row Column",
+        options: [
+            "Row Column",
+            "Row",
+            "Column",
+            "Row-reverse",
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "In J.S the Equal sign(=) is called?",
+        answer: "Assignment Operator",
+        options: [
+            "Listener",
+            "Variable",
+            "Assignment Operator",
+            "Under taker",
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "What is the new boy?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "What is used to create a String?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "What is used to bounce around?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 11,
+        question: "What is ?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 12,
+        question: "What is used?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 13,
+        question: "What is used to ?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 14,
+        question: "What is used to create ?",
+        answer: "Double or Single Quotes",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
+        ]
+    },
+
+    {
+        numb: 15,
+        question: "What is used to create a c?",
+        answer: "Double ",
+        options: [
+            "Double or Single commas",
+            "Double or Single hyphen",
+            "Double or Single semi-colon",
+            "Double or Single Quotes",
         ]
     },
 
--- script.js
+++ script.js
@@ -19,4 +19,37 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo");  //hide the info box
     quiz_box.classList.add("activeQuiz");  //show the quiz box
+    showQuestions(0);
 }
+
+let que_count = 0;
+
+const next_btn =document.querySelector('.next_btn');
+
+//if next button clicked 
+next_btn.onclick = ()=>{
+  if(que_count < questions.length - 1){
+    que_count++;
+    showQuestions(que_count);
+  }else{
+    console.log("Questions completed");
+  }
+    
+}
+
+//getting questions and options from array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].numb + "." + questions[index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+}
+
+const bottom_ques_counter = quiz_box.querySelector(".total_que");
+let totalQuesCountTag =  '<span><p>'+ que_count +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+bottom_ques_counter.innerHTML = totalQuesCountTag;
